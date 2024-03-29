9e24481673657ed539ed0be7dbd6301f2a3488b3
Rebecca Feeley <G00425652@atu.ie>
2024-03-29 23:16:37 +0000

    updating task 4 and gitignore

---
+++ assignment04-github.py
@@ -0,0 +1,7 @@
+# Write a program in python that will read a file from a repository, 
+# The program should then replace all the instances of the text "Andrew" with your name. 
+# The program should then commit those changes and push the file back to the repository.
+
+import requests 
+import urllib.parse
+
