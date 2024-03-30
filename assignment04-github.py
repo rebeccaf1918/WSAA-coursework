6f3aed94231671b1108573266ab98489812c880a
Rebecca Feeley <G00425652@atu.ie>
2024-03-30 01:49:09 +0000

    updating task 4

--- assignment04-github.py
+++ assignment04-github.py
@@ -4,4 +4,27 @@
 
 import requests 
 import urllib.parse
+from config import apikey as cfg  #importing the required apikey from config.py
+from github import Github
+
+
+old_name = "Andrew"
+new_name = "Rebecca"
+
+apikey = cfg["githubkey"]
+g = Github(apikey)
+
+repos = g.get_repo("rebeccaf1918/WSAA-coursework")
+file_path = "rebeccaf1918/WSAA-coursework/name.txt"
+contents = repos.get_contents(file_path)
+file_content = contents.decoded_content.decode("utf-8")
+
+def replace_name(file_content):
+    updated_content = file_content.replace(old_name, new_name)
+    return updated_content
+
+updated_content = replace_name(file_content)
+
+# Update the file in the repository
+repos.update_file(file_path, f"Assignment 04: Updated name from {old_name} to {new_name}", updated_content, contents.sha)
 
