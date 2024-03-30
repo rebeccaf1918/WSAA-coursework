43f90f2c71d9ad7ba488f8a4c02dc8ec63f77d27
Rebecca Feeley <G00425652@atu.ie>
2024-03-30 03:23:05 +0000

    completing task 4

--- assignment04-github.py
+++ assignment04-github.py
@@ -7,24 +7,21 @@ import urllib.parse
 from config import apikey as cfg  #importing the required apikey from config.py
 from github import Github
 
-
-old_name = "Andrew"
-new_name = "Rebecca"
+url = "https://api.github.com/repos/rebeccaf1918/WSAA-coursework"
 
 apikey = cfg["githubkey"]
 g = Github(apikey)
 
-repos = g.get_repo("rebeccaf1918/WSAA-coursework")
-file_path = "rebeccaf1918/WSAA-coursework/name.txt"
-contents = repos.get_contents(file_path)
-file_content = contents.decoded_content.decode("utf-8")
+repo = g.get_repo("rebeccaf1918/WSAA-coursework") # getting the repository so I can interact with it
+contents = repo.get_contents("name.txt") # Get the file contents from the specified repository
+file_content = contents.decoded_content.decode("utf-8") # using utf-8 to decode from bytes to strings
 
-def replace_name(file_content):
-    updated_content = file_content.replace(old_name, new_name)
+def replace_name(file_content): # defining a function to complete the changing of the name Andrew to Rebecca
+    updated_content = file_content.replace("Andrew", "Rebecca") # replacing instances of Andrew with Rebecca
     return updated_content
 
-updated_content = replace_name(file_content)
+updated_content = replace_name(file_content) 
 
-# Update the file in the repository
-repos.update_file(file_path, f"Assignment 04: Updated name from {old_name} to {new_name}", updated_content, contents.sha)
+# Update the file in the repository with the changes included with a commit messsage 
+repo.update_file(contents.path, f"Assignment 04: Updated name", updated_content, contents.sha, branch = "main")
 
