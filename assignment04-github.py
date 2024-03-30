7df37475c13892b27ad9f4365d1f7a178b9b5b06
Rebecca Feeley <G00425652@atu.ie>
2024-03-30 03:24:26 +0000

    finishing task 4

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,3 +1,6 @@
+# assignment04-github.py
+# Author: Rebecca Feeley
+
 # Write a program in python that will read a file from a repository, 
 # The program should then replace all the instances of the text "Andrew" with your name. 
 # The program should then commit those changes and push the file back to the repository.
